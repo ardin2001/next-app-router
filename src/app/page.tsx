271ca422d7276cab5dbf994db63a24550fb803a8
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-03 09:32:45 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center pt-10">
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <h2>Learning App Route</h2>
+      <p>Next JS</p>
+    </main>
+  );
+}
---
+++ src/app/setting/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./header";
+import Footer from "./footer";
+const Setting = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Setting</h1>
+            <Footer/>
+        </div>
+    );
+};
+export default Setting

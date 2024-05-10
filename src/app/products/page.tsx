337457738a893dca4e8c90f3d93dc3d2d34e496f
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-10 12:16:23 +0700

    test fetch api

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image"
 
-// async function fetchData() {
-//     // default : cache : 'force-cache'
-//     const response = await fetch(`${process.env.HOSTNAME_P1}/api/products`,{cache:'force-cache'})
-//     const {data} = await response.json()
-//     return data
-// }
+async function fetchData() {
+    // default : cache : 'force-cache'
+    const response = await fetch(`${process.env.HOSTNAME_P1}/api/products`,{cache:'force-cache'})
+    const {data} = await response.json()
+    return data
+}
 export default async function Products() {
-    // const products = await fetchData()
+    const products = await fetchData()
     
     return (
         <div className='grid grid-cols-5 gap-5 grid-flow-row w-11/12 mx-auto justify-between mt-5'>
-            {/* {products.map((product:any) => (
+            {products.map((product:any) => (
                 <div key={product.id} className=' p-2 bg-slate-400 rounded-md'>
                 <Image src={product.image} alt="" width={800} height={800} />
                 <p>{product.name}</p>
@@ -19,7 +19,7 @@ export default async function Products() {
                 <p>{product.price}</p>
                 <button className='bg-red-500 px-2 py-1 text-white' >Delete</button>
             </div>
-            ))} */}
+            ))}
         </div>
     )
 }

790e6c20d047462627ffbaee1c988e1d844a9294
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-10 14:17:17 +0700

    add route get all and get detail product

--- src/app/products/loading/@modal/(.)detail/[id]/page.tsx
+++ src/app/products/loading/@modal/(.)detail/[id]/page.tsx
@@ -8,10 +8,10 @@ export default async function Detail({params}:{params:{id:string}}) {
     return (
         <Modal>
             <div className="w-1/5 p-2 bg-slate-400 text-white relative left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <Image src={data[0].image} width={5000} height={5000} alt="" />
-                <h1>{data[0].name}</h1>
-                <p>{data[0].category}</p>
-                <p className="px-3 bg-green-400 text-center">{data[0].price}</p>
+                <Image src={data.image} width={5000} height={5000} alt="" />
+                <h1>{data.name}</h1>
+                <p>{data.category}</p>
+                <p className="px-3 bg-green-400 text-center">{data.price}</p>
             </div>
         </Modal>
     )

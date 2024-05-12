9242a00e5deb026535cd32cbb2d455053a7f6dc5
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-12 11:33:30 +0700

    update 8

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -4,14 +4,14 @@ import * as argon2 from "argon2";
 
 export async function POST(req:NextRequest){
     const inputUser = await req.json()
-    const response:any = await GetByAtribute("users",inputUser);
-    if(!response.status){
-        return NextResponse.json({status:false,message:"Invalid Email",data:response.data})        
-    }
+    // const response:any = await GetByAtribute("users",inputUser);
+    // if(!response.status){
+    //     return NextResponse.json({status:false,message:"Invalid Email",data:response.data})        
+    // }
 
-    const hash = await argon2.verify(response?.data[0]?.password, inputUser.password);
-    if(hash){
-        return NextResponse.json({status:true,message:"Login Success",data:response.data})        
-    }
+    // const hash = await argon2.verify(response?.data[0]?.password, inputUser.password);
+    // if(hash){
+    //     return NextResponse.json({status:true,message:"Login Success",data:response.data})        
+    // }
     return NextResponse.json({status:false,message:"Invalid Password",data:null})
 }

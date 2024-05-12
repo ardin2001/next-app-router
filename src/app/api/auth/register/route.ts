378d3f48d1b0ecdc7d58399b697b751cf4b025be
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-12 11:44:29 +0700

    memperbaiki error vercel

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,7 +2,7 @@ import { NextRequest,NextResponse } from "next/server";
 import { PostData } from "@/app/lib/firebase/FetchData";
 
 export async function POST(req:NextRequest){
-    // const inputUser = await req.json()
+    const inputUser = await req.json()
     // inputUser.verified = false
     // const response = await PostData("users",inputUser);
     // if(response.status){

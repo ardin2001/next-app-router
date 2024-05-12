0c4b89569156c725897037f0d0addf6e5c13bd33
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-12 12:10:52 +0700

    memperbaiki error vercel 7

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest,NextResponse } from "next/server";
-// import { PostData } from "@/app/lib/firebase/FetchData";
+import { PostData } from "@/app/lib/firebase/FetchData";
 
 export async function POST(req:NextRequest){
     const inputUser = await req.json()
-    // inputUser.verified = false
-    // const response = await PostData("users",inputUser);
-    // if(response.status){
-    //     return NextResponse.json({status:true,message:"Register Success",data:response.data})        
-    // }
+    inputUser.verified = false
+    const response = await PostData("users",inputUser);
+    if(response.status){
+        return NextResponse.json({status:true,message:"Register Success",data:response.data})        
+    }
     return NextResponse.json({status:false,message:"Register Failed",data:null})
 }
--- src/app/lib/firebase/FetchData.ts
+++ src/app/lib/firebase/FetchData.ts
@@ -32,3 +32,39 @@ export async function GetById(document: string, id: string) {
   }
   return result;
 }
+
+export async function PostData(document: string, addData: any) {
+  if (typeof addData == "string") {
+    addData = JSON.parse(addData);
+  }
+
+  let condition: any = {};
+  if (document == "users") {
+    condition.email = addData.email;
+  }
+
+  const q = query(
+    collection(db, document),
+    where("email", "==", addData.email)
+  );
+  const querySnapshot = await getDocs(q);
+  const data: any = querySnapshot.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  });
+
+  if (data.length == 0) {
+    if (document == "users" && addData.verified == false) {
+      const hash = await argon2.hash(addData.password);
+      addData.password = hash;
+    } else {
+      addData.verified = true;
+    }
+    const docRef = await addDoc(collection(db, document), addData);
+    addData.password = "********";
+    return { status: true, data: { id: docRef.id, ...addData } };
+  }
+  return { status: false, data: null };
+}

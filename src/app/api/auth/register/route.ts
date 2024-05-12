32983e88b6a58e38cc117bb67008b9224e456e17
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-12 12:15:16 +0700

    memperbaiki error vercel 8

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -4,9 +4,9 @@ import { PostData } from "@/app/lib/firebase/FetchData";
 export async function POST(req:NextRequest){
     const inputUser = await req.json()
     inputUser.verified = false
-    const response = await PostData("users",inputUser);
-    if(response.status){
-        return NextResponse.json({status:true,message:"Register Success",data:response.data})        
-    }
-    return NextResponse.json({status:false,message:"Register Failed",data:null})
+    // const response = await PostData("users",inputUser);
+    // if(response.status){
+    //     return NextResponse.json({status:true,message:"Register Success",data:response.data})        
+    // }
+    return NextResponse.json({status:false,message:"Register Failed",data:inputUser})
 }

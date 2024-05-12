6ca23ec78deee9083b5e43719d4a51f5f5f2019f
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-12 10:47:01 +0700

    update 3

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -28,18 +28,18 @@ const authOptions:NextAuthOptions  = {
           password: string;
         };
 
-        const response = await fetch(`${process.env.HOSTNAME_P1}/api/auth/login`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-        })
+        // const response = await fetch(`${process.env.HOSTNAME_P1}/api/auth/login`, {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({ email, password }),
+        // })
 
-        const {status,data} = await response.json()
-        if(status){
-          return data[0]
-        }
+        // const {status,data} = await response.json()
+        // if(status){
+        //   return data[0]
+        // }
         return null;
       },
     }),
--- src/app/products/loading/page.tsx
+++ src/app/products/loading/page.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image"
 import Link from "next/link"
 
-async function fetchData() {
-    await fetch('https://fakestoreapi.com/products', {cache: 'no-store'})
-    // const response = await fetch('http://localhost:3000/api/products' : test with wrong url fetch
-    const response = await fetch(`${process.env.HOSTNAME_P1}/api/products`, {
-        cache: 'no-store'
-    })
-    const { data } = await response.json()
-    return data
-}
+// async function fetchData() {
+//     await fetch('https://fakestoreapi.com/products', {cache: 'no-store'})
+//     // const response = await fetch('http://localhost:3000/api/products' : test with wrong url fetch
+//     const response = await fetch(`${process.env.HOSTNAME_P1}/api/products`, {
+//         cache: 'no-store'
+//     })
+//     const { data } = await response.json()
+//     return data
+// }
 
 export default async function Products() {
-    const products = await fetchData()
+    // const products = await fetchData()
 
     return (
         <div className='grid grid-cols-5 gap-5 grid-flow-row w-11/12 mx-auto justify-between mt-5'>
-            {products.map((product: any) => (
+            {/* {products.map((product: any) => (
                 <Link href={`${process.env.HOSTNAME_P1}/products/loading/detail/`+product.id} key={product.id} className=' p-2 bg-slate-400 rounded-md'>
                     <Image src={product.image} alt="" width={800} height={800} />
                     <p>{product.name}</p>
@@ -24,7 +24,7 @@ export default async function Products() {
                     <p>{product.price}</p>
                     <button className='bg-red-500 px-2 py-1 text-white' >Delete</button>
                 </Link>
-            ))}
+            ))} */}
         </div>
     )
 }

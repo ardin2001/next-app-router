20b2619ac334bce443df6d7e78126bde9db28bbd
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-12 10:39:10 +0700

    update 2

--- src/app/products/loading/@modal/(.)detail/[id]/page.tsx
+++ src/app/products/loading/@modal/(.)detail/[id]/page.tsx
@@ -1,17 +1,17 @@
 import Modal from '@/app/components/modal'
 import Image from 'next/image'
 export default async function Detail({params}:{params:{id:string}}) {
-    const response = await fetch(`${process.env.HOSTNAME_P1}/api/products?id=${params.id}`, {
-        cache: 'no-store'
-    })
-    const { data } = await response.json()
+    // const response = await fetch(`${process.env.HOSTNAME_P1}/api/products?id=${params.id}`, {
+    //     cache: 'no-store'
+    // })
+    // const { data } = await response.json()
     return (
         <Modal>
             <div className="w-1/5 p-2 bg-slate-400 text-white relative left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <Image src={data.image} width={5000} height={5000} alt="" />
+                {/* <Image src={data.image} width={5000} height={5000} alt="" />
                 <h1>{data.name}</h1>
                 <p>{data.category}</p>
-                <p className="px-3 bg-green-400 text-center">{data.price}</p>
+                <p className="px-3 bg-green-400 text-center">{data.price}</p> */}
             </div>
         </Modal>
     )

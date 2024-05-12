e835cd13bc4965e07e9d91bb8cef36675e6c64da
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-12 11:28:01 +0700

    update 7

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,11 +2,11 @@ import { NextRequest,NextResponse } from "next/server";
 import { PostData } from "@/app/lib/firebase/FetchData";
 
 export async function POST(req:NextRequest){
-    // const inputUser = await req.json()
-    // inputUser.verified = false
-    // const response = await PostData("users",inputUser);
-    // if(response.status){
-    //     return NextResponse.json({status:true,message:"Register Success",data:response.data})        
-    // }
+    const inputUser = await req.json()
+    inputUser.verified = false
+    const response = await PostData("users",inputUser);
+    if(response.status){
+        return NextResponse.json({status:true,message:"Register Success",data:response.data})        
+    }
     return NextResponse.json({status:false,message:"Register Failed",data:null})
 }
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,11 +4,11 @@ import { GetAll,GetById } from "@/app/lib/firebase/FetchData";
 export async function GET(req:NextRequest) {
   const id = req.nextUrl.searchParams.get('id')
   let data:any
-  if(id){
-    data = await GetById('products',id)
-  }else{
-    data = await GetAll('products')
-  }
+  // if(id){
+  //   data = await GetById('products',id)
+  // }else{
+  //   data = await GetAll('products')
+  // }
   
   return NextResponse.json({
     status: true,

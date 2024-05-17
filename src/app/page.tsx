855d8ad72a129aeff8cb6d079f92f131d55accbe
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-18 05:14:43 +0700

    add meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
-import Head from "next/head";
+import type { Metadata, ResolvingMetadata } from 'next'
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+// export const metadata: Metadata = {
+//   title: 'My Next app',
+//   description: "This's application use help everyone",
+//   icons: {
+//     icon: '/next.svg',
+//   }
+// }
 
-export default function Home() {
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  let id = params.id
+  id = 'A478zLgzIbsAtfKj5D2R'
+
+  // fetch data
+  const product = await fetch(`https://next-app-router-jet.vercel.app/api/products?id=${id}`).then((res) => res.json())
+  console.log(product.data.image)
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: product.data.name,
+    openGraph: {
+      images: product.data.image ? [product.data.image, ...previousImages] : previousImages,
+    },
+  }
+}
+
+export default function Home({ params, searchParams }: Props) {
   return (
     <main className="flex flex-col items-center pt-10">
-      <Head>
-        <title>Dashboard</title>
-      </Head>
       <h2>Learning App Route</h2>
       <p>Next JS</p>
     </main>
